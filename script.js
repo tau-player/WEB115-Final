ed768f6015cead6dd3fef140df3013d89a5d02fd
tau-player <jalenrwalker08@gmail.com>
2026-04-29 14:07:31 -0400

    Day 2 (3) commits
    Refactor card creation logic and implement a game class with shuffling functionality.

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const div=document.getElementById("playSpace");
 let p = document.createElement('p'); // placeholder test element
 div.appendChild(p); // placeholder test element
-const cards=['HB','OS','TS','RS','FS','IS','SS','VS','GS','NS','ES','JS','QS','KS','AS','OH','TH','RH','FH','IH','SH','VH','GH','NH','EH','JH','QH','KH','AH','OD','TD','RD','FD','ID','SD','VD','GD','ND','ED','JD','QD','KD','AD','OC','TC','RC','FC','IC','SC','VC','GC','NC','EC','JC','QC','KC','AC'];
-/*O,T,R,F,I,S,V,G,N,E,J,K,Q,A,S,H,D,C
-One, Two, thRee, Four, fIve, Six, seVen, eiGht, Nine, tEn, Jack, Queen, King, Ace, Spades, Hearts, Diamonds, Clubs
-ex: 5 of spades --> IS, 3 of diamonds --> RD, 8 of clubs --> GC, 10 of hearts --> EH*/
+const suits=["clubs", "diamonds", "hearts", "spades"];
+const ranks=["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"];
+let cards=[];
 
-for(let i=0; i<cards.length; i++){
-    p.innerHTML+=cards[i]+'.png'
+for(let i=0; i<4; i++){
+    for(let j=0; j<13; j++){
+        cards.push(ranks[j]+'_'+suits[i]+'.png')
+    }
 }
+
+console.log(cards)
+class game{
+    constructor(cards, size){
+        this.cards=cards
+        this.deck=[];
+        this.size=size;
+    }
+
+    shuffle(){
+        this.deck.push(this.cards)
+        
+        for(let i = this.deck.length -1; i>0; i--){
+            let j = Math.floor(Math.random()*(i+1));
+            let k = this.deck[i];
+            this.deck[i] = this.deck[j];
+            this.deck[j] = k;
+        }
+        console.log(this.deck)
+    }
+}
+
+let t1 = new game(cards,1);
+
+t1.shuffle()
